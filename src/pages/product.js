9400faf6c0048e646a79d4d80a09c5c06396438c
MAC <mac@MACs-iMac-2.local>
2021-08-01 17:36:44 +0300

    hosted api

--- src/pages/product.js
+++ src/pages/product.js
@@ -14,6 +14,7 @@ import {
 } from "./../reduxStore/actions/cart";
 import { addToCart } from "./../reduxStore/actions/cart";
 import { Ordering } from "./../reduxStore/actions/order";
+import ProductSlide from "../components/productSlide";
 
 const Product = (props) => {
   const [product, setProduct] = useState([]);
